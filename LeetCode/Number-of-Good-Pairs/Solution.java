e2e8b761e3df67372b7f2551e07ff5c0232d8aab
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-07-29 06:52:00 +0530

    Add solution for Number of Good Pairs

--- LeetCode/Number-of-Good-Pairs/Solution.java
+++ LeetCode/Number-of-Good-Pairs/Solution.java
@@ -1,11 +1,17 @@
-1int numIdenticalPairs(int* nums, int numsSize) {
-2    int count = 0;
-3    for(int i=0 ; i<numsSize; i++){
-4        for(int j=i+1 ;  j<numsSize ; j++){
-5            if(nums[i] == nums[j]){
-6                count++;
-7            }
-8        }
-9    }
-10    return count;
-11}
+1class Solution {
+2    public int numIdenticalPairs(int[] nums) {
+3        int freq[] = new int[101];
+4        for(int i=0;i<nums.length;i++){
+5            freq[nums[i]]++;
+6        }
+7        int total=0;
+8        for(int ele : freq){
+9            int pairs=0;
+10            if(ele>=2){
+11                pairs = (ele*(ele-1))/2;
+12            }
+13            total=total+pairs;
+14        }
+15        return total;
+16    }
+17}
