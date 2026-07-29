f1adc4b360f6ceb41b0b4ac21774d34b91973fe8
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-07-29 22:46:24 +0530

    Add solution for Alternating Digit Sum

--- LeetCode/Alternating-Digit-Sum/Solution.java
+++ LeetCode/Alternating-Digit-Sum/Solution.java
@@ -1,23 +1,22 @@
 1class Solution {
 2    public int alternateDigitSum(int n) {
-3    int original=n;
-4    int count=0;
-5    while(n!=0){
-6        n=n/10;
-7        count++;
-8    }
-9    int sign;
-10    if(count%2==0) sign=-1;
-11    else sign=1;
-12    int sum=0;
-13    while(original!=0){
-14        int digit = original % 10;
-15        sum=sum+digit*sign;
-16        sign=-sign;
-17        original=original/10;
-18    }
-19    return sum;
-20    }
-21    
+3        int ori=n;
+4        int count=0;
+5        while(n!=0){
+6            n=n/10;
+7            count++;
+8        }
+9        int[] arr = new int[count];
+10        for(int i=arr.length-1;i>=0;i--){
+11            int digit = ori%10;
+12            arr[i]=digit;
+13            ori=ori/10;
+14        }
+15        int sum=0;
+16        for(int i=0;i<arr.length;i++){
+17            if(i%2==0) sum=sum+arr[i];
+18            else sum=sum-arr[i];
+19        }
+20        return sum;
+21    }
 22}
-23
