1870dfb4bde233a03f4a818fabcde695ec076ca5
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-07-29 14:33:07 +0530

    Add solution for Maximum Product of Two Digits

--- LeetCode/Maximum-Product-of-Two-Digits/Solution.java
+++ LeetCode/Maximum-Product-of-Two-Digits/Solution.java
@@ -1,28 +1,16 @@
-1int maxProduct(int n) {
-2    int count=0;
-3    int temp = n;
-4    int digit;
-5    while(temp>0){
-6        digit=temp%10;
-7        count++;
-8        temp=temp/10;
-9    }
-10    temp = n;
-11    int arr[count];
-12    for(int i=count-1;i>=0;i--){
-13        digit=temp%10;
-14        arr[i]=digit;
-15        temp=temp/10;
-16    }
-17    int curPro;
-18    int maxPro=0;
-19    for(int i=0;i<count;i++){
-20        for(int j=i+1;j<count;j++){
-21            curPro=arr[i]*arr[j];
-22            if(curPro>maxPro){
-23                maxPro=curPro;
-24            }
-25        }
-26    }
-27    return maxPro;   
-28}
+1class Solution {
+2    public int maxProduct(int n) {
+3        String str = Integer.toString(n);
+4        int count = str.length();
+5        int[] arr = new int[count];
+6        int i=0;
+7        while(n!=0){
+8            int digit = n%10;
+9            arr[i]=digit;
+10            n=n/10;
+11            i++;
+12        }
+13        Arrays.sort(arr);
+14        return (arr[arr.length-1]*arr[arr.length-2]);
+15    }
+16}
