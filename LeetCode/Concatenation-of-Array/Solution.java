5fcfc6bbf530f0268b5e6f432d1d811383155f94
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-07-29 18:28:44 +0530

    Add solution for Concatenation of Array

--- LeetCode/Concatenation-of-Array/Solution.java
+++ LeetCode/Concatenation-of-Array/Solution.java
@@ -1,10 +1,10 @@
 1class Solution {
 2    public int[] getConcatenation(int[] nums) {
-3        int[] newArr = new int[nums.length*2];
-4        for(int i=0;i<nums.length;i++){
-5            newArr[i]=nums[i];
-6            newArr[i+nums.length]=nums[i];
-7        }
-8        return newArr;
+3       int[] ans = new int[2*nums.length];
+4       for(int i=0;i<nums.length;i++){
+5        ans[i] = nums[i];
+6        ans[i+nums.length] = nums[i];
+7       }
+8       return ans;
 9    }
 10}
