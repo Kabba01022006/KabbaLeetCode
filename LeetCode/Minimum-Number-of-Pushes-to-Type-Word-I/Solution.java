a0026c2615da8879890e3148e9e1e0239f51ae83
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-07-30 06:22:39 +0530

    Add solution for Minimum Number of Pushes to Type Word I

--- LeetCode/Minimum-Number-of-Pushes-to-Type-Word-I/Solution.java
+++ LeetCode/Minimum-Number-of-Pushes-to-Type-Word-I/Solution.java
@@ -1,20 +1,19 @@
 1class Solution {
 2    public int minimumPushes(String word) {
 3        int n=word.length();
-4        int clicks=1;
-5        int total=0;
-6        if(n<=8){
-7            total=n;
-8        }
-9        else if(n>8 && n<=16){
-10            total=8+(n-8)*2;
-11        }
-12        else if(n>16 && n<=24){
-13            total=24+(n-16)*3;
-14        }
-15        else if(n>24 && n<=26){
-16            total=48+(n-24)*4;
-17        }
-18        return total;
-19    }
-20}
+4        int total=0;
+5        if(n<=8){
+6            total=n;
+7        }
+8        else if(n>8 && n<=16){
+9            total=8+(n-8)*2;
+10        }
+11        else if(n>16 && n<=24){
+12            total=24+(n-16)*3;
+13        }
+14        else if(n>24 && n<=26){
+15            total=48+(n-24)*4;
+16        }
+17        return total;
+18    }
+19}
