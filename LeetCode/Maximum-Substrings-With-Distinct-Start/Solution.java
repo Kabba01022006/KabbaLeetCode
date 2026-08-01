0987c39dfdd7ce0da54b9f024dc090c6d42e0619
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-02 04:33:58 +0530

    Add solution for Maximum Substrings With Distinct Start

--- LeetCode/Maximum-Substrings-With-Distinct-Start/Solution.java
+++ LeetCode/Maximum-Substrings-With-Distinct-Start/Solution.java
@@ -1,9 +1,13 @@
 1class Solution {
 2    public int maxDistinct(String s) {
 3        HashSet<Character> set = new HashSet<>();
-4        for(int i=0;i<s.length();i++){
-5            set.add(s.charAt(i));
-6        }
-7        return set.size();
-8    }
-9}
+4        int count=0;
+5        for(int i=0;i<s.length();i++){
+6            if(!set.contains(s.charAt(i))){
+7                count++;
+8                set.add(s.charAt(i));
+9            }
+10        }
+11        return count;
+12    }
+13}
