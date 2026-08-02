f124c9e690613b180b81d8c85d87a153dbe53226
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-03 00:05:49 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.java
+++ LeetCode/Valid-Anagram/Solution.java
@@ -1,13 +1,17 @@
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
 3        if(s.length()!=t.length()) return false;
-4        char[] s1 = s.toCharArray();
-5        char[] s2 = t.toCharArray();
-6        Arrays.sort(s1);
-7        Arrays.sort(s2);
-8        for(int i=0;i<s1.length;i++){
-9            if(s1[i]!=s2[i]) return false;
-10        }
-11        return true;
-12    }
-13}
+4        int[] freq1 = new int[26];
+5        int[] freq2 = new int[26];
+6        for(int i=0;i<s.length();i++){
+7            freq1[s.charAt(i)-'a']++;
+8        }
+9        for(int i=0;i<t.length();i++){
+10            freq2[t.charAt(i)-'a']++;
+11        }
+12        for(int i=0;i<26;i++){
+13            if(freq1[i]!=freq2[i]) return false;
+14        }
+15        return true;
+16    }
+17}
