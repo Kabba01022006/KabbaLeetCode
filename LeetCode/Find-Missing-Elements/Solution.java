957f75de3f9e9fe8c072d1cc43b5b9f17b6109cd
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-04 08:34:25 +0530

    Add solution for Find Missing Elements

--- LeetCode/Find-Missing-Elements/Solution.java
+++ LeetCode/Find-Missing-Elements/Solution.java
@@ -1,19 +1,21 @@
 1class Solution {
 2    public List<Integer> findMissingElements(int[] nums) {
-3        HashSet<Integer> set = new HashSet<>();
+3        int[] freq = new int[101];
 4        List<Integer> list = new ArrayList<>();
 5        int min = Integer.MAX_VALUE;
 6        int max = Integer.MIN_VALUE;
 7        for(int i=0;i<nums.length;i++){
 8            min = Math.min(min,nums[i]);
 9            max = Math.max(max,nums[i]);
-10            set.add(nums[i]);
-11        }
-12        for(int i=min;i<=max;i++){
-13            if(!(set.contains(i))){
-14                list.add(i);
-15            }
-16        }
-17        return list;
-18    }
-19}
+10        }
+11        for(int i=0;i<nums.length;i++){
+12            freq[nums[i]]++;
+13        }
+14        for(int i=min;i<=max;i++){
+15            if(freq[i]==0){
+16                list.add(i);
+17            }
+18        }
+19        return list;
+20    }
+21}
