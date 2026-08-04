430eb5607ee650f1976ca865fb1a19ef914d24a9
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-04 08:34:47 +0530

    Add solution for Find Missing Elements

--- LeetCode/Find-Missing-Elements/Solution.java
+++ LeetCode/Find-Missing-Elements/Solution.java
@@ -5,17 +5,15 @@
 5        int min = Integer.MAX_VALUE;
 6        int max = Integer.MIN_VALUE;
 7        for(int i=0;i<nums.length;i++){
-8            min = Math.min(min,nums[i]);
-9            max = Math.max(max,nums[i]);
-10        }
-11        for(int i=0;i<nums.length;i++){
-12            freq[nums[i]]++;
-13        }
-14        for(int i=min;i<=max;i++){
-15            if(freq[i]==0){
-16                list.add(i);
-17            }
-18        }
-19        return list;
-20    }
-21}
+8            freq[nums[i]]++;
+9            min = Math.min(min,nums[i]);
+10            max = Math.max(max,nums[i]);
+11        }
+12        for(int i=min;i<=max;i++){
+13            if(freq[i]==0){
+14                list.add(i);
+15            }
+16        }
+17        return list;
+18    }
+19}
