079e5e6abb9da26ab1f39e586ad2b80f72da6a47
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-05 09:47:33 +0530

    Add solution for Intersection of Two Arrays II

--- LeetCode/Intersection-of-Two-Arrays-II/Solution.java
+++ LeetCode/Intersection-of-Two-Arrays-II/Solution.java
@@ -1,26 +1,26 @@
 1class Solution {
 2    public int[] intersect(int[] nums1, int[] nums2) {
 3        ArrayList<Integer> list = new ArrayList<>();
-4        Arrays.sort(nums1);
-5        Arrays.sort(nums2);
-6        int len1 = nums1.length;
-7        int len2 = nums2.length;
-8        int i=0;
-9        int j=0;
-10        while(i<len1 && j<len2){
-11            if(nums1[i]!=nums2[j]){
-12                if(nums1[i]<nums2[j]) i++;
-13                else j++;
-14            }
-15            else{
-16                list.add(nums1[i]);
-17                i++;
-18                j++;
+4        HashMap<Integer,Integer> map1 = new HashMap<>();
+5        HashMap<Integer,Integer> map2 = new HashMap<>();
+6        for(int ele:nums1){
+7            map1.put(ele,map1.getOrDefault(ele,0)+1);
+8        }
+9        for(int ele:nums2){
+10            map2.put(ele,map2.getOrDefault(ele,0)+1);
+11        }
+12        for(int ele:map1.keySet()){
+13            if(map2.containsKey(ele)){
+14                int freq = Math.min(map1.get(ele),map2.get(ele));
+15                while(freq!=0){
+16                    list.add(ele);
+17                    freq--;
+18                }
 19            }
 20        }
 21        int[] ans = new int[list.size()];
-22        for(int k=0;k<list.size();k++){
-23            ans[k]=list.get(k);
+22        for(int i=0;i<list.size();i++){
+23            ans[i]=list.get(i);
 24        }
 25        return ans;
 26    }
