f5959a630f36d21265e567333ce051700275a665
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-10 04:51:33 +0530

    Add solution for Palindrome Number

--- LeetCode/Palindrome-Number/Solution.java
+++ LeetCode/Palindrome-Number/Solution.java
@@ -1,12 +1,19 @@
-1class Solution {
-2    public boolean isPalindrome(int x) {
-3        int ori=x;
-4        int rev=0;
-5        while(ori!=0){
-6            rev=rev*10+ori%10;
-7            ori/=10;
-8        }
-9        if(x>0) return rev==x;
-10        else return rev==-1*x;
-11    }
-12}
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+}
