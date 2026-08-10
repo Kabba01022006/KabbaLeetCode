4fabdb365d73e67089295ca8460493e15996895e
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-10 20:50:17 +0530

    Add solution for Flipping an Image

--- LeetCode/Flipping-an-Image/Solution.java
+++ LeetCode/Flipping-an-Image/Solution.java
@@ -3,18 +3,14 @@
 3        for(int i=0;i<image.length;i++){
 4            int left=0;
 5            int right=image[0].length-1;
-6            while(left<right){
-7                int temp = image[i][left];
-8                image[i][left] = image[i][right];
+6            while(left<=right){ //because we want to invert the middle element too in case of odd
+7                int temp = 1-image[i][left];
+8                image[i][left] = 1-image[i][right];
 9                image[i][right] = temp;
 10                left++;
 11                right--;
 12            }
-13            for(int j=0;j<image[0].length;j++){
-14                if(image[i][j]==0) image[i][j]=1;
-15                else image[i][j] = 0;
-16            }
-17        }
-18        return image;
-19    }
-20}
+13        }
+14        return image;
+15    }
+16}
