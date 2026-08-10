eeb77214b1b61046c52eabe1a7d6f22d0bc3d03b
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-10 20:37:23 +0530

    Add solution for Reshape the Matrix

--- LeetCode/Reshape-the-Matrix/Solution.java
+++ LeetCode/Reshape-the-Matrix/Solution.java
@@ -1,30 +1,19 @@
 1class Solution {
 2    public int[][] matrixReshape(int[][] mat, int r, int c) {
-3
+3        if(r*c != mat.length*mat[0].length) return mat;
 4        int rows = mat.length;
 5        int cols = mat[0].length;
-6
-7        if(rows*cols != r*c) return mat;
-8        int[][] ans = new int[r][c];
-9        int[] flat = new int[rows * cols];
-10        int k=0;
-11        while(k<flat.length){
-12            for(int i=0;i<rows;i++){
-13                for(int j=0;j<cols;j++){
-14                flat[k]=mat[i][j];
-15                k++;
-16                }
-17            }
-18        }
-19        k=0;
-20        while(k<flat.length){
-21            for(int i=0;i<r;i++){
-22                for(int j=0;j<c;j++){
-23                    ans[i][j] = flat[k];
-24                    k++;
-25                }
-26            }
-27        }
-28        return ans;
-29    }
-30}
+6        int ans[][] = new int[r][c];
+7        for(int i=0;i<rows;i++){
+8            for(int j=0;j<cols;j++){
+9                int index = (i * cols) + j ; //index = currentRow * cols + currentColumn 
+10                //by doing this we get the flattenedIndex
+11                ans[index / c][index % c] = mat[i][j] ;
+12
+13                //rowNumber = Index/noOfColumns
+14                //colNumber = Index%noOfColumns
+15            }
+16        }
+17        return ans;
+18    }
+19}
