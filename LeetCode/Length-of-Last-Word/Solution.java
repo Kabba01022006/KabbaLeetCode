4ffa7dd501cb884410d609480a7d5ddf9c2b3e0f
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-12 03:06:47 +0530

    Add solution for Length of Last Word

--- LeetCode/Length-of-Last-Word/Solution.java
+++ LeetCode/Length-of-Last-Word/Solution.java
@@ -1,29 +1,10 @@
 1class Solution {
 2    public int lengthOfLastWord(String s) {
-3        char[] arr = s.toCharArray();
-4        int n=arr.length;
-5        int letterCount=0;
-6        int spaceCount=0;
-7        int i=n-1;
-8        if(arr[n-1]!=' '){
-9            while(i>=0 && arr[i]!=' '){
-10                letterCount++;
-11                i--;
-12            }
-13            return letterCount;
-14        }
-15        i=n-1;
-16        if(arr[n-1]==' '){
-17            while(i>=0 && arr[i]==' '){
-18                spaceCount++;
-19                i--;
-20            }
-21        }
-22        i=n-1-spaceCount;
-23        while(i>=0 && arr[i]!=' '){
-24            letterCount++;
-25            i--;
-26        }
-27        return letterCount;
-28    }
-29}
+3        s=s.trim();
+4        int i=s.length()-1;
+5        while(i>=0 && s.charAt(i)!=' '){
+6            i--;
+7        }
+8        return (s.length()-i-1);
+9    }
+10}
