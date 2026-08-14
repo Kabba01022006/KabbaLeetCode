1a09d09400c1caccf90a0be427da6ca66bb150e1
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-14 19:09:53 +0530

    Add solution for Max Consecutive Ones III

--- LeetCode/Max-Consecutive-Ones-III/Solution.java
+++ LeetCode/Max-Consecutive-Ones-III/Solution.java
@@ -1,20 +1,12 @@
-1class Solution {
-2    public int longestOnes(int[] nums, int k) {
-3        int i=0;
-4        int j=0;
-5        int len=0;
-6        int maxLen=0;
-7        int zeroCount=0;
-8        while (j < nums.length) {
-9            if (nums[j] == 0) zeroCount++;
-10            while (zeroCount > k) {
-11                if (nums[i] == 0)
-12                zeroCount--;
-13                i++;
-14            }
-15            maxLen = Math.max(maxLen, j - i + 1);
-16            j++;
-17        }
-18        return maxLen;
-19    }
-20}
+[1,1,1,0,0,0,1,1,1,1,0]
+2
+[0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+3
+[0,0,0,1]
+4
+[0,0,1,1]
+1
+[0,0,1,1,1,0,0] 
+0 
+[0,0,0,0]
+0
