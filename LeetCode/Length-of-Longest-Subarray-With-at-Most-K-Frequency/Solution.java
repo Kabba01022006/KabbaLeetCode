4febcb6545ed878f802775506fb920db85f6adb7
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-14 19:03:53 +0530

    Add solution for Length of Longest Subarray With at Most K Frequency

--- LeetCode/Length-of-Longest-Subarray-With-at-Most-K-Frequency/Solution.java
+++ LeetCode/Length-of-Longest-Subarray-With-at-Most-K-Frequency/Solution.java
@@ -1,18 +1,34 @@
-1class Solution {
-2    public int maxSubarrayLength(int[] nums, int k) {
-3        int left = 0 ;
-4        int right = 0 ;
-5        int maxLen=0;
-6        HashMap<Integer,Integer> map = new HashMap<>();
-7        while(right<nums.length){
-8            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
-9            while(map.getOrDefault(nums[right],0)>k){
-10                map.put(nums[left],map.getOrDefault(nums[left],0)-1);
-11                left++;
-12            }
-13            maxLen = Math.max(maxLen,right-left+1);
-14            right++;
-15        }
-16        return maxLen;
-17    }
-18}
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+       int maxVal = 0;
+       for(int num : nums){
+        maxVal = Math.max(maxVal, num);
+       }
+       if(maxVal <= 100000){
+        int[] freq = new int[maxVal + 1];
+        int left = 0;
+        int maxLen = 0;
+        for(int right = 0; right < nums.length; right++){
+            freq[nums[right]]++;
+            while(freq[nums[right]] > k){
+                freq[nums[left]]--;
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+       }
+       Map<Integer, Integer> freq = new HashMap<>();
+       int left = 0;
+       int maxLen = 0;
+       for(int right = 0; right < nums.length; right++){
+        freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+        while(freq.get(nums[right]) > k){
+            freq.put(nums[left], freq.get(nums[left]) - 1);
+            left++;
+        }
+        maxLen = Math.max(maxLen, right - left + 1);
+       }
+       return maxLen;
+    }
+}
