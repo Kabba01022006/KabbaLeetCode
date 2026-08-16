0b1722d990258de9f7c0d49a6743824ffef03ecb
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-17 00:04:53 +0530

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.java
+++ LeetCode/Valid-Palindrome/Solution.java
@@ -1,18 +1,14 @@
 1class Solution {
 2    public boolean isPalindrome(String s) {
-3        StringBuilder str = new StringBuilder();
-4        for(int i=0;i<s.length();i++){
-5            if(Character.isLetterOrDigit(s.charAt(i))){
-6                str.append(Character.toLowerCase(s.charAt(i)));
-7            }
-8        }
-9        int left=0;
-10        int right=str.length()-1;
-11        while(left<right){
-12            if(str.charAt(left)!=str.charAt(right)) return false;
-13            left++;
-14            right--;
-15        }
-16        return true;
-17    }
-18}
+3        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+4        char[] ori = s.toCharArray();
+5        int i=0;
+6        int j=ori.length-1;
+7        while(i<j){
+8            if(ori[i]!=ori[j]) return false;
+9            i++;
+10            j--;
+11        }
+12        return true;
+13    }
+14}
