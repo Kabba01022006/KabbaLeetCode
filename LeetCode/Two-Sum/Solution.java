548f4f27574fbe58da1a79d73fbbaecc7bed799c
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-19 12:21:18 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -1,12 +1,17 @@
- for(var i =0;i<nums.length;i++){
-    var item = nums[i];   
-    for(var j=0; j < nums.length;j++){
-        var item2 = nums[j];
-        var sum = item + item2;
-        
-    if(sum === target ){
-      return [i,j];
-      
-    }
-  }
- }
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int arr[] = new int[2];
+4        HashMap<Integer,Integer> map = new HashMap<>();
+5        for(int i=0;i<nums.length;i++){
+6            int comp = (target - nums[i]);
+7            if(map.containsKey(comp)){
+8                arr[0]=map.get(comp);
+9                arr[1]=i;
+10            }
+11            else{
+12                map.put(nums[i],i);
+13            }
+14        }
+15        return arr;
+16    }
+17}
