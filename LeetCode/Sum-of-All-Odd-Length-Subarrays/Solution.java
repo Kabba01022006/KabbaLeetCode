0d5ca7d3c14075346c72b445214001c694675245
shobhitkabba <125171824+Kabba01022006@users.noreply.github.com>
2026-08-20 13:52:36 +0530

    Add solution for Sum of All Odd Length Subarrays

--- LeetCode/Sum-of-All-Odd-Length-Subarrays/Solution.java
+++ LeetCode/Sum-of-All-Odd-Length-Subarrays/Solution.java
@@ -1,17 +1,16 @@
 1class Solution {
 2    public int sumOddLengthSubarrays(int[] arr) {
-3        int totalSum=0;
+3        int sum=0;
 4        for(int i=0;i<arr.length;i++){
-5            int sum=0;
-6            for(int j=i;j<arr.length;j++){
-7                sum+=arr[j];
-8
-9                if((j - i + 1) % 2 != 0) {
-10                totalSum += sum;
+5            for(int j=i;j<arr.length;j++){
+6                int dist=j-i+1;
+7                if(dist%2!=0){
+8                    for(int k=i;k<=j;k++){
+9                        sum+=arr[k];
+10                    }
 11                }
 12            }
 13        }
-14        return totalSum;
+14        return sum;
 15    }
 16}
-17
